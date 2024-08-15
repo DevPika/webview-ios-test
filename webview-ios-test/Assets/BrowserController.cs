236c41601a28b20de58c76f4f5b614e827bba49d
Ayaskant Panigrahi <ayaskant.panigrahi@gmail.com>
2024-08-15 18:15:01 +0530

    Try adding SFSafariViewController
    https://discussions.unity.com/t/sfsafariviewcontroller-and-oauth-flows/671589/7

---
+++ webview-ios-test/Assets/BrowserController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public static class SFSafariView {
+#if UNITY_IOS
+    [DllImport("__Internal")]
+    extern static void launchUrl(string url);
+    [DllImport("__Internal")]
+    extern static void dismiss();
+#endif
+
+    public static void LaunchUrl(string url) {
+#if UNITY_IOS
+        launchUrl(url);
+#endif
+    }
+
+    public static void Dismiss() {
+#if UNITY_IOS
+        dismiss();
+#endif
+    }
+}
+
+public class BrowserController : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_IOS
+        LaunchUrl("https://google.com");
+#endif
+    }
+
+    //void Update()
+    //{
+
+    //}
+
+    void OnAuthCompleted(string param) {
+        Debug.Log("Browser Controller OnAuthCompleted: " + param);
+    }
+
+    void OnInitialLoadCompleted(string param) {
+        Debug.Log("Browser Controller OnInitialLoadCompleted: " + param);
+    }
+}
